828e892823aefdb0c07bf34997fb4440202c6a46
Connor Ashley <ashleycm@vcu.edu>
2021-07-12 18:58:07 -0400

    added palette and seedColors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
 import Palette from './Palette';
+import seedColors from './seedColors';
 
 function App() {
   return (
-    <div className="App">
-      <Palette />
+    <div>
+      <Palette palette={seedColors[4]} />
     </div>
   );
 }
