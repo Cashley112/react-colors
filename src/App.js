921ed33309db518d3dec0eded47b5a6c927a33d6
Connor Ashley <ashleycm@vcu.edu>
2021-07-14 10:18:56 -0400

    added react rou router and set up basic routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Palette from './Palette';
+import { Route, Switch } from 'react-router-dom';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 function App() {
   console.log(generatePalette(seedColors[4]))
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => <h1>Palette List Goes Here</h1>} />
+      <Route exact path='/palette/:id' render={() => <h1>Palette List Goes Here</h1>} />
+    </Switch>
   );
 }
 
