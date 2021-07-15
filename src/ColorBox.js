0483af64e8592afb642094d637e8ecd738ed0bd6
Connor Ashley <ashleycm@vcu.edu>
2021-07-15 19:03:21 -0400

    finished refactor of ColorBox.js and .css

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -24,6 +24,17 @@ const styles = {
     colorName: {
         color: props => chroma(props.background).luminance() <= 0.09 ? 'white' : 'black'
     },
+    boxContent: {
+        position: 'absolute',
+        width: '100%',
+        left: '0px',
+        bottom: '0px',
+        padding: '10px',
+        color: 'black',
+        letterSpacing: '1px',
+        textTransform: 'uppercase',
+        fontSize: '12px'
+    },
     seeMore: {
         color: props => chroma(props.background).luminance() >= 0.5 ? 'rgba(0, 0, 0, 0.6)' : 'white',
         background: 'rgba(255, 255, 255, 0.3)',
@@ -56,6 +67,56 @@ const styles = {
         border: 'none',
         textDecoration: 'none',
         opacity: '0'
+    },
+    copyOverlay: {
+        opacity: '0',
+        zIndex: '0',
+        width: '100%',
+        height: '100%',
+        transition: 'transform 0.6s ease-in-out',
+        transform: 'scale(0.1)'
+    },
+    showOverlay: {
+        opacity: '1',
+        transform: 'scale(50)',
+        zIndex: '10',
+        position: 'absolute'
+    },
+    copyMsg: {
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        right: '0',
+        bottom: '0',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        fontSize: '4rem',
+        transform: 'scale(0.1)',
+        opacity: '0',
+        color: 'white',
+        '& h1': {
+            fontWeight: '400',
+            textShadow: '1px 2px black',
+            background: 'rgba(255, 255, 255, 0.2)',
+            width: '100%',
+            textAlign: 'center',
+            marginBottom: '0',
+            padding: '1rem',
+            textTransform: 'uppercase'
+        },
+        '& p': {
+            fontSize: '2rem',
+            fontWeight: '100'
+        }
+    },
+    showMsg: {
+        opacity: '1',
+        transform: 'scale(1)',
+        zIndex: '25',
+        transition: 'all 0.4s ease-in-out',
+        transitionDelay: '0.3s'
     }
 }
 
@@ -73,21 +134,19 @@ class ColorBox extends Component {
     render() {
         const { name, background, paletteId, id, showingFullPalette, classes } = this.props;
         const { copied } = this.state;
-        const isDarkColor = chroma(background).luminance() <= 0.09;
-        const isLightColor = chroma(background).luminance() >= 0.7;
         return(
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className={classes.colorBox}>
                 <div 
                     style={{ background }} 
-                    className={`copy-overlay ${copied && 'show'}`} 
+                    className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} 
                 />
-                <div className={`copy-msg ${copied && 'show'}`}>
+                <div className={`${classes.copyMsg} ${copied && classes.showMsg}`}>
                     <h3>COPIED!</h3>
                     <p className={classes.copyText}>{this.props.background}</p>
                 </div>
-                <div className='copy-container'>
-                    <div className='box-content'>
+                <div>
+                    <div className={classes.boxContent}>
                         <span className={classes.colorName}>{name}</span>
                     </div>
                     <button className={classes.copyButton}>Copy</button>
