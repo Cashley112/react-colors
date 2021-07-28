e6aee42132373512dd458fcece87c6f51478b3f1
Connor Ashley <ashleycm@vcu.edu>
2021-07-28 12:02:06 -0400

    moved JSS out into new styles folder, created constants.js

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -3,28 +3,8 @@ import { ChromePicker } from 'react-color';
 import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import { withStyles } from '@material-ui/styles';
 
-import Button from '@material-ui/core/Button'
-
-const styles = {
-    root: {
-        width: '100%'
-    },
-    picker: {
-        // refactor
-        width: '100% !important',
-        marginTop: '2rem'
-    },
-    addColor: {
-        width: '100%',
-        padding: '1rem',
-        marginTop: '1rem',
-        fontSize: '2rem'
-    },
-    colorNameInput: {
-        width: '100%',
-        height: '70px'
-    }
-}
+import Button from '@material-ui/core/Button';
+import styles from './styles/ColorPickerFormStyles';
 
 function ColorPickerForm(props) {
     const { colors, classes, setColors, paletteIsFull } = props;
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -10,46 +10,8 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button'
 
-import { makeStyles } from '@material-ui/core/styles';
 import PaletteMetaForm from './PaletteMetaForm';
-
-const drawerWidth = 400;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex'
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  navBtns: {
-    marginRight: '1rem',
-    '& a': {
-      textDecoration: 'none'
-    }
-  },
-  button: {
-    margin: '0 0.5rem',
-  }
-}))
-
+import useStyles from './styles/PaletteFormNavStyles';
 
 export default function PaletteFormNav(props) {
     const classes = useStyles();
